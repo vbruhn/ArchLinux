9db8f7e98a36cb73f41c8c8b6a17ee4c8d6a665c
vbruhn <30861405+vbruhn@users.noreply.github.com>
2019-04-01 08:24:06 +0200

    Create 10-Start.sh
    Create Arch-Base System on mounted partitions ... ends with chroot

---
+++ 10-Start.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# run this script after mounting the partitions
+
+# configure the morrorlist
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+nano /etc/pacman.d/mirrorlist
+
+# install the base-system
+pacstrap /mnt base base-devel intel-ucode bash-completion
+# With WiFi:
+# pacstrap /mnt base base-devel intel-ucode bash-completion wpa_supplicant dialog
+
+# create fstab
+genfstab -U /mnt > /mnt/etc/fstab
+nano /mnt/etc/fstab
+
+arch-chroot /mnt/
